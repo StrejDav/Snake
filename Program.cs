e2c7af70f676fc7ebf965210060f899f27d87fe4
David Strejček <strejcek.david.1@gmail.com>
2021-10-15 22:19:46 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        const int MF_BYCOMMAND = 0x00000000;
+        const int SC_MINIMIZE = 0xF020;
+        const int SC_MAXIMIZE = 0xF030;
+        const int SC_SIZE = 0xF000;
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 10;
+            Console.WindowWidth = 20;
+
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MINIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MAXIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_SIZE, MF_BYCOMMAND);
+
+            Console.CursorVisible = false;
+            ConsoleKey playAgain;
+
+            Console.WriteLine("Zadej obtížnost: ");
+
+            bool cont = false;
+            int difficulty = 1;
+
+            while (!cont)
+            {
+                try
+                {
+                    difficulty = Int16.Parse(Console.ReadLine());
+                    cont = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Nezadali jste číslo. Zadejte číslo: ");
+                    cont = false;
+                }
+            }
+            int dt = 200;
+
+            do
+            {
+
+                Console.Clear();
+                Snake snake = new Snake(difficulty);
+                while (true)
+                {
+                    //dt = (int)(1000 * snake.GetDT());
+                    DateTime beginWait = DateTime.Now;
+                    while (!Console.KeyAvailable && DateTime.Now.Subtract(beginWait).TotalMilliseconds < dt) // waits for keypress
+                        Thread.Sleep(1);
+
+                    if (Console.KeyAvailable) // if key pressed
+                    {
+                        switch (Console.ReadKey().Key)
+                        {
+                            case ConsoleKey.UpArrow:
+                                snake.Move(new System.Numerics.Vector2(0, -1));
+                                break;
+                            case ConsoleKey.DownArrow:
+                                snake.Move(new System.Numerics.Vector2(0, 1));
+                                break;
+                            case ConsoleKey.LeftArrow:
+                                snake.Move(new System.Numerics.Vector2(-1, 0));
+                                break;
+                            case ConsoleKey.RightArrow:
+                                snake.Move(new System.Numerics.Vector2(1, 0));
+                                break;
+                        }
+                    }
+
+                    else snake.Move();
+
+                    if (snake.Collision())
+                        break;
+
+                    while ((DateTime.Now - beginWait).TotalMilliseconds < dt) // wait until the full 500 ms expire
+                        Thread.Sleep(1);
+
+                    snake.Render();
+                }
+
+                Console.Clear();
+                Console.WriteLine("Game over!");
+                Console.WriteLine("Your score was: {0}", snake.GetLength() - 2);
+                Console.WriteLine("Press any key to play again\nPress ESC to exit");
+                Thread.Sleep(1000);
+                playAgain = Console.ReadKey().Key;
+
+            } while (playAgain != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Snake
+{
+    class Snake
+    {
+        List<Vector2> snakeParts = new List<Vector2>();
+        List<Vector2> bombs = new List<Vector2>();
+        Vector2 fruit;
+        int difficulty = 1;
+
+        public int GetLength()
+        {
+            return snakeParts.Count;
+        }
+
+        public Snake(int diff)
+        {
+            snakeParts.Add(new Vector2(Console.WindowWidth / 2, Console.WindowHeight / 2));
+            snakeParts.Add(new Vector2(Console.WindowWidth / 2 + 1, Console.WindowHeight / 2));
+            snakeParts.Add(new Vector2(Console.WindowWidth / 2 + 2, Console.WindowHeight / 2));
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (Vector2 snakePart in snakeParts) // initial draw of the snake parts
+            {
+                Console.SetCursorPosition((int)snakePart.X, (int)snakePart.Y);
+                Console.Write('═');
+            }
+
+            Console.SetCursorPosition((int)snakeParts[^1].X, (int)snakeParts[^1].Y);
+            Console.Write(' ');
+
+            difficulty = diff;
+
+
+            GenerateFood(); // initial generation of food
+        }
+        
+        public void Render()
+        {
+            AteFood(); // checks if snake ate food; generates new food and bomb if it did
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.SetCursorPosition((int)snakeParts[^1].X, (int)snakeParts[^1].Y); // deletes last part of snake
+            Console.Write(' ');
+            Console.SetCursorPosition((int)snakeParts[0].X, (int)snakeParts[0].Y); // draw first part of snake
+            if (snakeParts[0].Y - snakeParts[1].Y == -1 & snakeParts[2].X - snakeParts[1].X == 1)
+            {
+                Console.Write("║");
+                Console.SetCursorPosition((int)snakeParts[1].X, (int)snakeParts[1].Y);
+                Console.Write("╚");
+            }
+            else if (snakeParts[0].Y - snakeParts[1].Y == -1 & snakeParts[2].X - snakeParts[1].X == -1)
+            {
+                Console.Write("║");
+                Console.SetCursorPosition((int)snakeParts[1].X, (int)snakeParts[1].Y);
+                Console.Write("╝");
+            }
+            else if (snakeParts[0].Y - snakeParts[1].Y == 1 & snakeParts[2].X - snakeParts[1].X == 1)
+            {
+                Console.Write("║");
+                Console.SetCursorPosition((int)snakeParts[1].X, (int)snakeParts[1].Y);
+                Console.Write("╔");
+            }
+            else if (snakeParts[0].Y - snakeParts[1].Y == 1 & snakeParts[2].X - snakeParts[1].X == -1)
+            {
+                Console.Write("║");
+                Console.SetCursorPosition((int)snakeParts[1].X, (int)snakeParts[1].Y);
+                Console.Write("╗");
+            }
+            else if (snakeParts[0].X - snakeParts[1].X == -1 & snakeParts[2].Y - snakeParts[1].Y == 1)
+            {
+                Console.Write("═");
+                Console.SetCursorPosition((int)snakeParts[1].X, (int)snakeParts[1].Y);
+                Console.Write("╗");
+            }
+            else if (snakeParts[0].X - snakeParts[1].X == 1 & snakeParts[2].Y - snakeParts[1].Y == 1)
+            {
+                Console.Write("═");
+                Console.SetCursorPosition((int)snakeParts[1].X, (int)snakeParts[1].Y);
+                Console.Write("╔");
+            }
+            else if (snakeParts[0].X - snakeParts[1].X == -1 & snakeParts[2].Y - snakeParts[1].Y == -1)
+            {
+                Console.Write("═");
+                Console.SetCursorPosition((int)snakeParts[1].X, (int)snakeParts[1].Y);
+                Console.Write("╝");
+            }
+            else if (snakeParts[0].X - snakeParts[1].X == 1 & snakeParts[2].Y - snakeParts[1].Y == -1)
+            {
+                Console.Write("═");
+                Console.SetCursorPosition((int)snakeParts[1].X, (int)snakeParts[1].Y);
+                Console.Write("╚");
+            }
+            else if (snakeParts[0].X - snakeParts[1].X == 0 & snakeParts[0].Y - snakeParts[1].Y != 0)
+            {
+                Console.Write("║");
+            }
+            else if (snakeParts[0].X - snakeParts[1].X != 0 & snakeParts[0].Y - snakeParts[1].Y == 0)
+            {
+                Console.Write("═");
+            }
+        }
+
+        public void Move()
+        {
+            snakeParts.Insert(0, snakeParts[0] + (snakeParts[0] - snakeParts[1]));
+            snakeParts.RemoveAt(snakeParts.Count - 1);
+        }
+
+        public void Move(Vector2 dir)
+        {
+            Vector2 firstPart = snakeParts[0] + dir;
+            if ((firstPart != snakeParts[1]) & (firstPart != snakeParts[0] + (snakeParts[0] - snakeParts[1])))
+            {
+                snakeParts.Insert(0, firstPart);
+            }
+            else
+            {
+                snakeParts.Insert(0, snakeParts[0] + (snakeParts[0] - snakeParts[1]));
+            }
+            snakeParts.RemoveAt(snakeParts.Count - 1);
+        }
+
+        public void AddPart()
+        {
+            Vector2 diff = snakeParts[^1] - snakeParts[^2];
+            Vector2 lastPart = snakeParts[^1];
+            snakeParts.Add(lastPart + diff);
+        }
+
+        public bool Collision()
+        {
+            if (snakeParts[0].X == -1 | snakeParts[0].Y == -1 | snakeParts[0].X == Console.WindowWidth | snakeParts[0].Y == Console.WindowHeight | bombs.Contains(snakeParts[0]))
+                return true;
+            for(int i = 1; i < snakeParts.Count; i++)
+                if (snakeParts[0] == snakeParts[i])
+                    return true;
+            return false;
+        }
+
+        private void AteFood()
+        {
+            if (snakeParts[0] == fruit)
+            {
+                AddPart();
+                GenerateBomb();
+                GenerateFood();
+            }
+        }
+
+        private void GenerateFood()
+        {
+            Random rnd = new Random();
+            fruit = new Vector2(rnd.Next(0, Console.WindowWidth), rnd.Next(0, Console.WindowHeight));
+            while (snakeParts.Contains(fruit) | bombs.Contains(fruit))
+                fruit = new Vector2(rnd.Next(0, Console.WindowWidth), rnd.Next(0, Console.WindowHeight));
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition((int)fruit.X, (int)fruit.Y);
+            Console.Write("♂");
+        }
+       
+        
+        private void GenerateBomb()
+        {
+            Random rnd = new Random();
+            for (int i = 1; i <= difficulty; i++)
+            {
+                Vector2 bomb = new Vector2(rnd.Next(0, Console.WindowWidth), rnd.Next(0, Console.WindowHeight));
+                while (snakeParts.Contains(bomb) | VectorDifferenceLength(snakeParts[0], bomb)! < 2)
+                    bomb = new Vector2(rnd.Next(0, Console.WindowWidth), rnd.Next(0, Console.WindowHeight));
+                bombs.Add(bomb);
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.SetCursorPosition((int)bomb.X, (int)bomb.Y);
+                Console.Write("♀");
+            }
+        }
+
+        public double GetDT()
+        {
+            double x = 1 / (double)snakeParts.Count;
+            if (snakeParts[0] - snakeParts[1] == new Vector2(0, 1) | snakeParts[0] - snakeParts[1] == new Vector2(0, -1))
+                return 1.5 * x;
+            return x;
+        }
+
+        private double VectorDifferenceLength(Vector2 vector1, Vector2 vector2)
+        {
+            return Math.Sqrt((vector1.X - vector2.X) * (vector1.X - vector2.X) + (vector1.Y - vector2.Y) * (vector1.Y - vector2.Y));
+
+        }
+    }
+}
